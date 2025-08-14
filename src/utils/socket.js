1b24a8dc5bb7cabb742c0836d8c16a9b465f1912
Ramesh136 <88448065+Ramesh136@users.noreply.github.com>
2025-08-14 13:00:41 +0530

    Update socket.js
    update socket path

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -5,6 +5,6 @@ export const createSocketConnection = () => {
   if (location.hostname === "localhost") {
     return io(BASE_URL);
   } else {
-    return io("/", { path: "/api/socket.io" });
+    return io("https://devtinderbackend-67oz.onrender.com", { path: "/socket.io" });
   }
 };
